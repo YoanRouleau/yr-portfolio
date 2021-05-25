f488d7964cef78f512c0144b6b0887c6abf17f81
YoanRouleau <yoan-rouleau@live.fr>
2021-05-25 22:58:55 +0200

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2332dcbbee39426cc39147f9d308311718beb8c5',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2332dcbbee39426cc39147f9d308311718beb8c5',
+    ),
+  ),
+);
